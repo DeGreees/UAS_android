4197abfdff52a05dbe6822142a3d05bf35f02384
DeGreees <darkbludevil@gmail.com>
2022-07-20 09:14:31 +0700

    benekno intent

--- app/src/main/java/com/bagusrasyidramadhaninugraha/utsa/uas_ppb1/freya.java
+++ app/src/main/java/com/bagusrasyidramadhaninugraha/utsa/uas_ppb1/freya.java
@@ -15,12 +15,11 @@ public class freya extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_freya);
-        editnama = findViewById(R.id.namplgliquid1);
-        editalamat = findViewById(R.id.alamatplgliquid1);
-        editbarang = findViewById(R.id.jmlbrgliquid1);
-        editpesanliquid1 = findViewById(R.id.editpesanliquid1);
-
-        button = findViewById(R.id.btnkirimliquid1);
+        editnama = findViewById(R.id.namplgliquid2);
+        editalamat = findViewById(R.id.alamatplgliquid2);
+        editbarang = findViewById(R.id.jmlbrgliquid2);
+        editpesanliquid1 = findViewById(R.id.editpesanliquid2);
+        button = findViewById(R.id.btnkirimliquid2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,14 +27,11 @@ public class freya extends AppCompatActivity {
                 String pesan2 = editalamat.getText().toString();
                 String pesan3 = editbarang.getText().toString();
                 String pesan4 = editpesanliquid1.getText().toString();
-
                 String semuapesan = "Nama: " + pesan + "\n" + "Alamat: " + pesan2 + "\n"
                       +  "Barang: " + pesan3 + "\n" + "Pesan Kepada Penjual: " + pesan4;
                 Intent kirimwa = new Intent(Intent.ACTION_SEND);
                 kirimwa.setType("text/plain");
                 kirimwa.putExtra(Intent.EXTRA_TEXT, semuapesan);
-
-
                 kirimwa.putExtra("jid", "6282141988172" + "@a.whatsapp.net");
                 kirimwa.setPackage("com.whatsapp");
                 startActivity(kirimwa);
--- app/src/main/java/com/bagusrasyidramadhaninugraha/utsa/uas_ppb1/hexohm.java
+++ app/src/main/java/com/bagusrasyidramadhaninugraha/utsa/uas_ppb1/hexohm.java
@@ -8,20 +8,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class    hexohm extends AppCompatActivity {
-
+public class  hexohm extends AppCompatActivity {
     Button button;
     EditText editnama, editalamat, editbarang, editpesanmods1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hexohm);
-
         editnama = findViewById(R.id.namplgmod1);
         editalamat = findViewById(R.id.alamatplgmod1);
         editbarang = findViewById(R.id.jmlbrgmod1);
         editpesanmods1 = findViewById(R.id.editpesanmod1);
-
         button = findViewById(R.id.btnkirimmod1);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,17 +27,13 @@ public class    hexohm extends AppCompatActivity {
                 String pesan2 = editalamat.getText().toString();
                 String pesan3 = editbarang.getText().toString();
                 String pesan4 = editpesanmods1.getText().toString();
-
                 String semuapesan = "Nama: " + pesan + "\n" + "Alamat: " + pesan2 + "\n"
-                       + "Barang: " + pesan3 + "\n" + "Pesan Kepada Penjual: " + pesan4;
+                        + "Barang: " + pesan3 + "\n" + "Pesan Kepada Penjual: " + pesan4;
                 Intent kirimwa = new Intent(Intent.ACTION_SEND);
                 kirimwa.setType("text/plain");
                 kirimwa.putExtra(Intent.EXTRA_TEXT, semuapesan);
-
-
                 kirimwa.putExtra("jid", "6282141988172" + "@a.whatsapp.net");
                 kirimwa.setPackage("com.whatsapp");
-
                 startActivity(kirimwa);
             }
         });
